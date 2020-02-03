55077921f88fae141fd473b5b8f13294b4f07dee
David Enciso <dpenciso@msn.com>
2020-02-03 11:43:14 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("open").style.display = "none"
+    document.getElementById("nav").style.width = "70%"
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0"
+    document.getElementById("open").style.display = "block"
+
+}
